695fa2dddbe394616129ac8fc2c12c2b941ed906
alexzirnea <alexzirnea@gmail.com>
2023-04-17 22:38:01 +0300

    Update jack to latest versiv1on

--- scripts/recipes/install_jack2.sh
+++ scripts/recipes/install_jack2.sh
@@ -10,7 +10,7 @@ fi
 echo "The current directory is: $(pwd)"
 echo "Cloning jack2 into $ZYNTHIAN_SW_DIR / $SW_DIR"
 
-git clone --branch v1.9.21 https://github.com/jackaudio/jack2.git $SW_DIR
+git clone --branch v1.9.22 https://github.com/jackaudio/jack2.git $SW_DIR
 cd jack2
 ./waf configure
 ./waf build
