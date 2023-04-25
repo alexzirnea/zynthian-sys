46c94e0c45143f9f6cb4623a4c53f4da6ddb4de5
alexzirnea <alexzirnea@gmail.com>
2023-04-26 01:13:20 +0300

    + Update qmidinet to get fixes from @riban.
    + Upgrade install LV2 scripts. Install not fully tested yet.

--- scripts/recipes/install_qmidinet.sh
+++ scripts/recipes/install_qmidinet.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# qmidinet
+# Needs deb:
+# libqt5svg5-dev
+
 cd $ZYNTHIAN_SW_DIR
 git clone https://github.com/rncbc/qmidinet.git
 cd qmidinet
-./autogen.sh
-./configure
+cmake -B build
+cd build
 make -j 3
 make install
-cd ..
+cd ../..
